41095cfa172957b65693cc3851752a4aedc17856
iReek <i_r_e_k@mail.ru>
2022-06-23 22:59:18 +0300

    Initial Commit

---
+++ 10Lesson_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10Lesson_HW
+//
+//  Created by Reek i on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
